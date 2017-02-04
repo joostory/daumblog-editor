d745fde2a5e2d93a712ddbc328b7f76c8dd00e81
Joo <joo@joostory.net>
2017-02-04 22:55:33 +0900

    2. oauth2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# daumblog-editor
+# DaumBlog Editor
+
+[다음 블로그 api](https://developers.daum.net/services/apis/blog)를 통해서 다음 블로그에 글을 작성/수정합니다.
--- src/main/main.js
+++ src/main/main.js
@@ -1,6 +1,8 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const url = require('url')
 const path = require('path')
+const fs = require('fs')
+const electronOauth2 = require('electron-oauth2')
 
 let mainWindow
 
@@ -15,6 +17,23 @@ app.on('ready', () => {
     protocol: 'file:',
     slashes: true
   }))
+
+  ipcMain.on("request-auth", (e) => {
+    let oauth2info = JSON.parse(fs.readFileSync(path.join(__dirname, "../../oauth2info.json"), 'utf8'))
+
+    const daumOauth = electronOauth2(oauth2info, {
+      alwaysOnTop: true,
+      autoHideMenuBar: true,
+      webPreferences: {
+        nodeIntegration: false
+      }
+    });
+
+    daumOauth.getAccessToken({})
+      .then(auth => {
+        e.sender.send('receive-auth', auth)
+      });
+  })
 })
 
 app.on('window-all-closed', () => {
